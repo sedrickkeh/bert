f0a73993a6e57dfa7f23b54bb4577b49fb5ee810
sedrick <sedrickkeh@gmail.com>
2019-05-26 01:41:16 +0800

    full accuracy computation

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,3 +1,34 @@
+def simple_accuracy(preds, labels):
+	return (preds == labels).mean()
+
+def full_accuracy(preds, labels):
+	# [exactly 1 match, exactly 2 matches, exactly 3 matches, exactly 4 matches]
+	num_same_array = [0,0,0,0]
+	for i in range(len(preds)):
+		curr_cnt = 0
+		if (preds[i][0] == labels[i][0]): curr_cnt += 1
+		if (preds[i][1] == labels[i][1]): curr_cnt += 1
+		if (preds[i][2] == labels[i][2]): curr_cnt += 1
+		if (preds[i][3] == labels[i][3]): curr_cnt += 1
+		num_same_array[curr_cnt-1] += 1
+	# cumulative counts 
+	# [at least 1 match, at least 2 matches, at least 3 matches, at least 4 matches]
+	num_same_array[1] += num_same_array[0]
+	num_same_array[2] += num_same_array[1]
+	num_same_array[3] += num_same_array[2]
+	return num_same_array
+
 def compute_metrics(preds, labels):
-    return {"acc": (preds == labels).mean()}
+	simple_acc = simple_accuracy(preds, labels)
+	metrics = {"simple_acc": simple_acc}
+
+	if (len(preds[0]) == 1):
+		return metrics
+	else:
+		full_acc = full_accuracy(preds, labels)
+		metrics["at_least_1_match"] = full_acc[0]
+		metrics["at_least_2_matches"] = full_acc[1]
+		metrics["at_leadt_3_matches"] = full_acc[2]
+		metrics["all_4_matches"] = full_acc[3]
+		return metrics
 
