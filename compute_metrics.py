a480f9603528e6b75eb319a1e2cd659646a15221
sedrick <sedrickkeh@gmail.com>
2019-05-26 01:52:47 +0800

    full accuracy computation

--- compute_metrics.py
+++ compute_metrics.py
@@ -13,9 +13,11 @@ def full_accuracy(preds, labels):
 		num_same_array[curr_cnt-1] += 1
 	# cumulative counts 
 	# [at least 1 match, at least 2 matches, at least 3 matches, at least 4 matches]
-	num_same_array[1] += num_same_array[0]
-	num_same_array[2] += num_same_array[1]
-	num_same_array[3] += num_same_array[2]
+	num_same_array[2] += num_same_array[3]
+	num_same_array[1] += num_same_array[2]
+	num_same_array[0] += num_same_array[1]
+	for i in range(len(num_same_array)):
+		num_same_array[i] /= len(preds)
 	return num_same_array
 
 def compute_metrics(preds, labels):
@@ -26,9 +28,8 @@ def compute_metrics(preds, labels):
 		return metrics
 	else:
 		full_acc = full_accuracy(preds, labels)
-		metrics["at_least_1_match"] = full_acc[0]
-		metrics["at_least_2_matches"] = full_acc[1]
-		metrics["at_leadt_3_matches"] = full_acc[2]
+		metrics["at_least_1_match"] = full_acc[0] 
+		metrics["at_least_2_matches"] = full_acc[1] 
+		metrics["at_least_3_matches"] = full_acc[2]
 		metrics["all_4_matches"] = full_acc[3]
 		return metrics
-
