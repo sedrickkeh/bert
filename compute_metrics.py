939a04191c44a76b66c75d5a1620e56d2380f679
sedrick <sedrickkeh@gmail.com>
2019-05-26 16:11:30 +0800

    fixed accuracy measurements

--- compute_metrics.py
+++ compute_metrics.py
@@ -6,20 +6,22 @@ def simple_accuracy(preds, labels):
 	return cnt / len(preds)
 
 def full_accuracy(preds, labels):
-	# [exactly 1 match, exactly 2 matches, exactly 3 matches, exactly 4 matches]
-	num_same_array = [0,0,0,0]
+	# [0 matches, exactly 1 match, exactly 2 matches, exactly 3 matches, exactly 4 matches]
+	num_same_array = [0,0,0,0,0]
 	for i in range(len(preds)):
 		curr_cnt = 0
 		if (preds[i][0] == labels[i][0]): curr_cnt += 1
 		if (preds[i][1] == labels[i][1]): curr_cnt += 1
 		if (preds[i][2] == labels[i][2]): curr_cnt += 1
 		if (preds[i][3] == labels[i][3]): curr_cnt += 1
-		num_same_array[curr_cnt-1] += 1
+		num_same_array[curr_cnt] += 1
+
 	# cumulative counts 
-	# [at least 1 match, at least 2 matches, at least 3 matches, at least 4 matches]
+	# [0 matches, at least 1 match, at least 2 matches, at least 3 matches, at least 4 matches]
+	num_same_array[3] += num_same_array[4]
 	num_same_array[2] += num_same_array[3]
 	num_same_array[1] += num_same_array[2]
-	num_same_array[0] += num_same_array[1]
+
 	for i in range(len(num_same_array)):
 		num_same_array[i] /= len(preds)
 	return num_same_array
@@ -63,10 +65,10 @@ def compute_metrics(preds, labels, label_list):
 	else:
 		full_acc = full_accuracy(preds, labels)
 		mbti_acc = mbti_accuracy(preds, labels)
-		metrics["at_least_1_match"] = full_acc[0] 
-		metrics["at_least_2_matches"] = full_acc[1] 
-		metrics["at_least_3_matches"] = full_acc[2]
-		metrics["all_4_matches"] = full_acc[3]
+		metrics["at_least_1_match"] = full_acc[1] 
+		metrics["at_least_2_matches"] = full_acc[2] 
+		metrics["at_least_3_matches"] = full_acc[3]
+		metrics["all_4_matches"] = full_acc[4]
 		metrics["E/I"] = mbti_acc[0]
 		metrics["N/S"] = mbti_acc[1]
 		metrics["F/T"] = mbti_acc[2]
